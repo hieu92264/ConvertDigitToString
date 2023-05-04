b2e6941224b9e90f9689f5ddc436e13fc1bb55d6
hieu92264 <104044219+hieu92264@users.noreply.github.com>
2023-05-04 11:37:39 +0700

    Add project files.

---
+++ ConvertDigitToString/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ConvertDigitToString
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txt_input = new TextBox();
+            groupBox1 = new GroupBox();
+            richText_output = new RichTextBox();
+            BTN_Run = new Button();
+            BTN_Reset = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(22, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(160, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Nhập vào số nguyên N";
+            // 
+            // txt_input
+            // 
+            txt_input.Location = new Point(298, 26);
+            txt_input.Name = "txt_input";
+            txt_input.Size = new Size(100, 23);
+            txt_input.TabIndex = 1;
+            txt_input.KeyPress += txt_input_KeyPress;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(richText_output);
+            groupBox1.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.ForeColor = SystemColors.Highlight;
+            groupBox1.Location = new Point(22, 69);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(376, 125);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Kết quả đọc số";
+            // 
+            // richText_output
+            // 
+            richText_output.ForeColor = Color.Red;
+            richText_output.Location = new Point(6, 22);
+            richText_output.Name = "richText_output";
+            richText_output.ReadOnly = true;
+            richText_output.Size = new Size(364, 96);
+            richText_output.TabIndex = 0;
+            richText_output.Text = "";
+            // 
+            // BTN_Run
+            // 
+            BTN_Run.Location = new Point(169, 217);
+            BTN_Run.Name = "BTN_Run";
+            BTN_Run.Size = new Size(75, 23);
+            BTN_Run.TabIndex = 3;
+            BTN_Run.Text = "Thực hiện";
+            BTN_Run.UseVisualStyleBackColor = true;
+            BTN_Run.Click += BTN_Run_Click;
+            // 
+            // BTN_Reset
+            // 
+            BTN_Reset.Location = new Point(323, 217);
+            BTN_Reset.Name = "BTN_Reset";
+            BTN_Reset.Size = new Size(75, 23);
+            BTN_Reset.TabIndex = 4;
+            BTN_Reset.Text = "Xoá";
+            BTN_Reset.UseVisualStyleBackColor = true;
+            BTN_Reset.Click += BTN_Reset_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(436, 254);
+            Controls.Add(BTN_Reset);
+            Controls.Add(BTN_Run);
+            Controls.Add(groupBox1);
+            Controls.Add(txt_input);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txt_input;
+        private GroupBox groupBox1;
+        private RichTextBox richText_output;
+        private Button BTN_Run;
+        private Button BTN_Reset;
+    }
+}
---
+++ ConvertDigitToString/Form1.cs
@@ -0,0 +1,245 @@
+﻿namespace ConvertDigitToString
+{
+    public partial class Form1 : Form
+    {
+        bool check = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_input_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Nhập sai kiểu dữ liệu!");
+            }
+        }
+        private void dem_hang_tram(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        richText_output.Text = richText_output.Text + "Một trăm ";
+                        break;
+                    }
+                case 2:
+                    {
+                        richText_output.Text = richText_output.Text + "Hai trăm ";
+                        break;
+                    }
+                case 3:
+                    {
+                        richText_output.Text = richText_output.Text + "Ba trăm ";
+                        break;
+                    }
+                case 4:
+                    {
+                        richText_output.Text = richText_output.Text + "Bốn trăm ";
+                        break;
+                    }
+                case 5:
+                    {
+                        richText_output.Text = richText_output.Text + "Năm trăm ";
+                        break;
+                    }
+                case 6:
+                    {
+                        richText_output.Text = richText_output.Text + "Sáu trăm ";
+                        break;
+                    }
+                case 7:
+                    {
+                        richText_output.Text = richText_output.Text + "Bảy trăm ";
+                        break;
+                    }
+                case 8:
+                    {
+                        richText_output.Text = richText_output.Text + "Tám trăm ";
+                        break;
+                    }
+                case 9:
+                    {
+                        richText_output.Text = richText_output.Text + "Chín trăm ";
+                        break;
+                    }
+            }
+        }
+        private void dem_hang_chuc(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    {
+                        richText_output.Text = richText_output.Text + "linh ";
+                        break;
+                    }
+                case 1:
+                    {
+                        richText_output.Text = richText_output.Text + "mười ";
+                        break;
+                    }
+                case 2:
+                    {
+                        richText_output.Text = richText_output.Text + "hai mươi ";
+                        break;
+                    }
+                case 3:
+                    {
+                        richText_output.Text = richText_output.Text + "ba mươi ";
+                        break;
+                    }
+                case 4:
+                    {
+                        richText_output.Text = richText_output.Text + "bốn mươi ";
+                        break;
+                    }
+                case 5:
+                    {
+                        richText_output.Text = richText_output.Text + "năm mươi ";
+                        break;
+                    }
+                case 6:
+                    {
+                        richText_output.Text = richText_output.Text + "sáu mươi ";
+                        break;
+                    }
+                case 7:
+                    {
+                        richText_output.Text = richText_output.Text + "bảy mươi ";
+                        break;
+                    }
+                case 8:
+                    {
+                        richText_output.Text = richText_output.Text + "tám mươi ";
+                        break;
+                    }
+                case 9:
+                    {
+                        richText_output.Text = richText_output.Text + "chín mươi ";
+                        break;
+                    }
+            }
+        }
+        private void dem_hang_don_vi(int n, bool check)
+        {
+            int dv = n % 10;
+            n = n / 10;
+            int chuc = n % 10;
+            switch (dv)
+            {
+                case 0:
+                    {
+                        break;
+                    }
+                case 1:
+                    {
+                        if (chuc == 0 || check == true)
+                        {
+                            richText_output.Text = richText_output.Text + "một";
+                        }
+                        else richText_output.Text = richText_output.Text + "mốt";
+                        break;
+                    }
+                case 2:
+                    {
+                        richText_output.Text = richText_output.Text + "hai";
+                        break;
+                    }
+                case 3:
+                    {
+                        richText_output.Text = richText_output.Text + "ba";
+                        break;
+                    }
+                case 4:
+                    {
+                        richText_output.Text = richText_output.Text + "bốn";
+                        break;
+                    }
+                case 5:
+                    {
+                        richText_output.Text = richText_output.Text + "năm";
+                        break;
+                    }
+                case 6:
+                    {
+                        richText_output.Text = richText_output.Text + "sáu";
+                        break;
+                    }
+                case 7:
+                    {
+                        richText_output.Text = richText_output.Text + "bảy";
+                        break;
+                    }
+                case 8:
+                    {
+                        richText_output.Text = richText_output.Text + "tám";
+                        break;
+                    }
+                case 9:
+                    {
+                        richText_output.Text = richText_output.Text + "chín";
+                        break;
+                    }
+            }
+        }
+
+        private void BTN_Run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txt_input.Text == "") MessageBox.Show("Chưa nhập dữ liệu!");
+                else if (Convert.ToInt32(txt_input.Text) >= 1 && Convert.ToInt32(txt_input.Text) <= 999)
+                {
+                    int n = Convert.ToInt32(txt_input.Text);
+                    if (1 <= n && n <= 9)
+                    {
+                        check = true;
+                        this.dem_hang_don_vi(n, check);
+                    }
+                    else if (10 <= n && n <= 99)
+                    {
+                        int donvi = n % 10;
+                        n = n / 10;
+                        int chuc = n % 10;
+                        this.dem_hang_don_vi(donvi, check);
+                        this.dem_hang_chuc(chuc);
+                    }
+                    else if (100 <= n && n <= 999)
+                    {
+                        int donvi = n % 10;
+                        n = n / 10;
+                        int chuc = n % 10;
+                        n = n / 10;
+                        int tram = n % 10;
+                        this.dem_hang_tram(tram);
+                        this.dem_hang_chuc(chuc);
+                        this.dem_hang_don_vi(donvi, check);
+                    }
+                }
+                else MessageBox.Show("Số cần chuyển không hợp lệ!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BTN_Reset_Click(object sender, EventArgs e)
+        {
+            txt_input.Clear();
+            richText_output.Clear();
+        }
+    }
+}
